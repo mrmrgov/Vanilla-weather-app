55f804095d1f04c58a265f23be0f6700c17b7bb8
Rajitha Mohankumar <mrmrgov@gmail.com>
2021-02-18 15:49:34 -0800

    completed API

---
+++ src/app.js
@@ -0,0 +1,39 @@
+
+function formatDate(timestamp)
+{
+   let date = new Date(timestamp);
+   let hours = date.getHours();
+   if (hours<10)
+   {
+       hours = `0${hours}`
+   }
+   let minutes = date.getMinutes();
+    if (minutes<10)
+   {
+       minutes = `0${minutes}`
+   }
+   let days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+   let day = days[date.getDay()];
+   return`${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response)
+{
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML =response.data.main.temp;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML =response.data.name;
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML =response.data.weather[0].description;
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML =response.data.main.humidity;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML =Math.round(response.data.wind.speed);
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML =formatDate(response.data.dt * 1000);
+}
+
+let apiKey="5bee267bee9b93771611ea21812bd265";
+let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric` ;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
